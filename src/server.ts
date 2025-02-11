21ffd217140efdf800c29074334129cc037f9730
Varun-18 <varunshah735@gmail.com>
2025-02-11 15:49:50 +0530

    11-02-25 localhost request accepted

--- src/server.ts
+++ src/server.ts
@@ -38,12 +38,11 @@ app.use(cookieParser());
  */
 app.use(
   cors({
-    origin: ['https://made-on-you.vercel.app'],
+    origin: ['https://made-on-you.vercel.app', 'http://localhost:3000'],
     credentials: true,
     methods: 'GET, POST, PUT, DELETE, OPTIONS',
     allowedHeaders:
       'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-      
   })
 );
 
